e6fe736f89a42823a78a65fe9531ea252be6da33
everyshare-code <138933190+everyshare-code@users.noreply.github.com>
2024-08-08 19:59:58 +0900

    load_dotenv 삭제

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 from core.init_app import create_app
 import uvicorn
 from core.config import settings
-from dotenv import load_dotenv
-load_dotenv()
 app = create_app()
 
 if __name__ == "__main__":
